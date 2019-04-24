a0137ee36204c18daa5975991e5fda540d75f5ee
prestontewell <preston@tewellphoto.com>
2019-04-24 09:46:25 -0500

    add username for signup

--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -11,6 +11,10 @@
           <input type="text" class="form-control" v-model="name">
         </div>
         <div class="form-group">
+          <label>User Name:</label> 
+          <input type="text" class="form-control" v-model="username">
+        </div>
+        <div class="form-group">
           <label>Email:</label>
           <input type="email" class="form-control" v-model="email">
         </div>
@@ -35,6 +39,7 @@ export default {
   data: function() {
     return {
       name: "",
+      username: "",
       email: "",
       password: "",
       passwordConfirmation: "",
@@ -45,6 +50,7 @@ export default {
     submit: function() {
       var params = {
         name: this.name,
+        username: this.username,
         email: this.email,
         password: this.password,
         password_confirmation: this.passwordConfirmation
